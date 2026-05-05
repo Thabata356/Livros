3eaf5dbb6db7e4dbf9501929f101b0e150b7c492
Thabata356 <thabatav7@gmail.com>
2026-05-05 14:08:50 -0300

    :bug: fix: Corrigir integração do gráfico e nomes das tabelas no dashboard.

--- web-data-viz/src/models/medidaModel.js
+++ web-data-viz/src/models/medidaModel.js
@@ -1,29 +1,28 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idAquario, limite_linhas) {
+function buscarUltimasMedidas(idLivro, limite_linhas) {
 
+    //Alterei o fk_aquario para livro
     var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    FROM medida
-                    WHERE fk_aquario = ${idAquario}
-                    ORDER BY id DESC LIMIT ${limite_linhas}`;
+        paginas_lidas as paginas, 
+                        data_registro,
+                        DATE_FORMAT(data_registro,'%H:%i:%s') as momento_grafico
+                    FROM historico_leitura
+                    WHERE fk_livro = ${idLivro}
+                    ORDER BY id_historico DESC LIMIT ${limite_linhas}`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function buscarMedidasEmTempoReal(idAquario) {
+function buscarMedidasEmTempoReal(idLivro) {
 
     var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        FROM medida WHERE fk_aquario = ${idAquario} 
-                    ORDER BY id DESC LIMIT 1`;
+        paginas_lidas as paginas,
+                        DATE_FORMAT(data_registro,'%H:%i:%s') as momento_grafico, 
+                        fk_livro 
+                        FROM historico_leitura WHERE fk_livro = ${idLivro} 
+                    ORDER BY id_historico DESC LIMIT 1`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
